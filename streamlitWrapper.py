fdb36dcfd35d28520f22f80c01632854470c5e04
Zeeshan Hyder <109053785+Zeeshann1@users.noreply.github.com>
2023-01-04 15:43:54 +0800

    Update streamlitWrapper.py

--- streamlitWrapper.py
+++ streamlitWrapper.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 process = subprocess.Popen(["streamlit", "run", os.path.join(
-            'C:/Users/zeeshan/Downloads/webcam-streaming-main/webcam-streaming-main/app-Copy.py')])
+            'C:/Users/zeeshan/Downloads/webcam-streaming-main/webcam-streaming-main/app.py')])
 
 #OR
 
